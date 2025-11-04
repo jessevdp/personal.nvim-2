18115ec1d3a82b121e9bcedddd65077af7c55f08
Jesse van der Pluijm <jessevdp@hey.com>
2025-11-04 09:12:42 +0100

    Setup minimal initial nvim config

---
+++ lua/config/globals.lua
@@ -0,0 +1,28 @@
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.updatetime = 200
+
+vim.opt.undofile = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+
+vim.opt.cursorline = true
+
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+vim.opt.mouse = "a"
+
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+vim.opt.breakindentopt = "list:2"
+
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
