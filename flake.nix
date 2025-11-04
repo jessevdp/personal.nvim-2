de78cfba22b2484df92519701af8ab49d239a5d8
Jesse van der Pluijm <jessevdp@hey.com>
2025-11-04 09:13:24 +0100

    Setup nix flake with wrapped neovim (nightly)

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    wrappers = {
+      url = "github:lassulus/wrappers";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    wrappers,
+    neovim-nightly-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          neovim-nightly-overlay.overlays.default
+        ];
+      };
+
+      wrapNvim = import ./nix/wrap-nvim.nix {inherit wrappers;};
+    in {
+      packages.default =
+        (wrapNvim.apply {
+          inherit pkgs;
+          name = "nvim-wrapped-jessevdp";
+          configRoot = ./.;
+        }).wrapper;
+    });
+}
