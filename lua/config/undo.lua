b31a4cd9b1205e77620aa905180c0b77757b752f
Jesse van der Pluijm <jessevdp@hey.com>
2025-12-03 08:11:14 +0100

    Configure Undotree UI

--- lua/config/undo.lua
+++ lua/config/undo.lua
@@ -1,1 +1,12 @@
 vim.opt.undofile = true
+
+vim.cmd("packadd nvim.undotree")
+
+local function toggle_undotree()
+  require("undotree").open({
+    title = "Undotree",
+    command = "45vnew",
+  })
+end
+
+vim.keymap.set("n", "<leader>u", toggle_undotree)
