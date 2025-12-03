a578ad731b47e38bee2e6f53c2d52cdeb6e67892
Jesse van der Pluijm <jessevdp@hey.com>
2025-12-03 07:41:04 +0100

    Center current line in the screen after jumping

--- lua/config/scroll.lua
+++ lua/config/scroll.lua
@@ -1,2 +1,8 @@
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
+
+-- Center current line in the screen after jumping up/down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
