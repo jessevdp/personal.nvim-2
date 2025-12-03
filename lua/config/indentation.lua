4cf05e46a9aa5cffcf67faa3f4b25f5025d05326
Jesse van der Pluijm <jessevdp@hey.com>
2025-12-03 07:39:18 +0100

    Keep visual mode selection when using > or < to change indent

--- lua/config/indentation.lua
+++ lua/config/indentation.lua
@@ -1,3 +1,7 @@
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+
+-- Keep selection when changing indent in visual mode
+vim.keymap.set("v", ">", ">gv")
+vim.keymap.set("v", "<", "<gv")
