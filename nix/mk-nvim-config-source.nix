c1d1526585b8be97be37707682c93f545e2cb54d
Jesse van der Pluijm <jessevdp@hey.com>
2026-01-24 09:30:42 +0100

    chore: Nix formatting using alejandra

--- nix/mk-nvim-config-source.nix
+++ nix/mk-nvim-config-source.nix
@@ -7,26 +7,25 @@
 
     # see :h runtimepath / vimfiles
     "filetype.lua" # filetypes (:h new-filetype)
-    "autoload/"    # automatically loaded scripts (:h autoload-functions)
-    "colors/"      # color scheme files (:h colorscheme)
-    "compiler/"    # compiler files (:h compiler)
-    "doc/"         # documentation (:h write-local-help)
-    "ftplugin/"    # filetype plugins (:h write-filetype-plugin)
-    "indent/"      # indent scripts (:h indent-expression)
-    "keymap/"      # key mapping files (:h mbyte-keymap)
-    "lang/"        # menu translations (:h menutrans)
-    "lsp/"         # LSP client configurations (:h lsp-config)
-    "lua/"         # (:h Lua) plugins
-    "menu.vim"     # GUI menus (:h menu.vim)
-    "pack/"        # packages (:h packadd)
-    "parser/"      # treesitter syntax parsers (:h treesitter)
-    "plugin/"      # plugin scripts (:h write-plugin)
-    "queries/"     # treesitter queries (:h treesitter)
-    "rplugin/"     # remote-plugin scripts (:h remote-plugin)
-    "spell/"       # spell checking files (:h spell)
-    "syntax/"      # syntax files (:h mysyntaxfile)
-    "tutor/"       # tutorial files (:h Tutor)
-
+    "autoload/" #### automatically loaded scripts (:h autoload-functions)
+    "colors/" ###### color scheme files (:h colorscheme)
+    "compiler/" #### compiler files (:h compiler)
+    "doc/" ######### documentation (:h write-local-help)
+    "ftplugin/" #### filetype plugins (:h write-filetype-plugin)
+    "indent/" ###### indent scripts (:h indent-expression)
+    "keymap/" ###### key mapping files (:h mbyte-keymap)
+    "lang/" ######## menu translations (:h menutrans)
+    "lsp/" ######### LSP client configurations (:h lsp-config)
+    "lua/" ######### (:h Lua) plugins
+    "menu.vim" ##### GUI menus (:h menu.vim)
+    "pack/" ######## packages (:h packadd)
+    "parser/" ###### treesitter syntax parsers (:h treesitter)
+    "plugin/" ###### plugin scripts (:h write-plugin)
+    "queries/" ##### treesitter queries (:h treesitter)
+    "rplugin/" ##### remote-plugin scripts (:h remote-plugin)
+    "spell/" ####### spell checking files (:h spell)
+    "syntax/" ###### syntax files (:h mysyntaxfile)
+    "tutor/" ####### tutorial files (:h Tutor)
   ];
 
   relativeEntries = map (entry: lib.path.append root entry) entries;
