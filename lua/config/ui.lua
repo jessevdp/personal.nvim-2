d43158c1d527dddf29051e1e345ac5f5fcb9e1de
Jesse van der Pluijm <jessevdp@hey.com>
2026-01-24 13:32:42 +0100

    Conditionally show colorcolumn at 80, 120 chars

--- lua/config/ui.lua
+++ lua/config/ui.lua
@@ -4,3 +4,20 @@ vim.opt.signcolumn = "yes"
 
 vim.opt.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+vim.pack.add({
+  { src = "https://github.com/m4xshen/smartcolumn.nvim" },
+}, { confirm = false })
+
+require("smartcolumn").setup({
+  colorcolumn = { 80, 120 },
+  scope = "window",
+  disabled_filetypes = {
+    "checkhealth",
+    "help",
+    "markdown",
+    "nvim-pack",
+    "nvim-undotree",
+    "text",
+  },
+})
