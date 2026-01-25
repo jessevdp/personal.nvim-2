4f66875111f9d822eb9225e3a188a43a397e7eca
Jesse van der Pluijm <jessevdp@hey.com>
2026-01-25 10:57:44 +0100

    Show git status of files in Oil file explorer

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -7,6 +7,15 @@ require("gruvbox").setup({
   highlight_groups = {
     LspReferenceRead = { bg = "bg2" },
     PreInsert = { fg = "gray" },
+
+    OilGitAdded = { fg = "green_dark" },
+    OilGitModified = { fg = "yellow_dark" },
+    OilGitRenamed = { fg = "blue_dark" },
+    OilGitDeleted = { fg = "red_dark" },
+    OilGitCopied = { fg = "blue_dark" },
+    OilGitConflict = { fg = "purple_dark" },
+    OilGitUntracked = { fg = "bg2" },
+    OilGitIgnored = { fg = "gray" },
   },
 })
 
