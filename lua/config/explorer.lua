2e25f52330a69f3647c63f78e5dd6c9670c84cdb
Jesse van der Pluijm <jessevdp@hey.com>
2026-01-25 10:57:44 +0100

    Show git status of files in Oil file explorer

--- lua/config/explorer.lua
+++ lua/config/explorer.lua
@@ -1,13 +1,15 @@
 vim.pack.add({
   { src = "https://github.com/stevearc/oil.nvim" },
+  { src = "https://github.com/malewicz1337/oil-git.nvim" },
 }, { confirm = false })
 
-local oil = require("oil")
-
-oil.setup({
+require("oil").setup({
   default_file_explorer = true,
   skip_confirm_for_simple_edits = true,
   watch_for_changes = true,
 })
 
+require("oil-git").setup({})
+
 vim.keymap.set("n", "-", "<cmd>Oil<CR>")
+
