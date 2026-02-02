91c21ffa8c4752b731008954a1de35c1e37a3690
Jesse van der Pluijm <jessevdp@hey.com>
2026-02-02 15:26:59 +0100

    Remove highlight overrides that are now part of gruvbox.nvim (after update)

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -6,16 +6,7 @@ require("gruvbox").setup({
   extend_background_behind_borders = true,
   highlight_groups = {
     LspReferenceRead = { bg = "bg2" },
-    PreInsert = { fg = "gray" },
-
-    OilGitAdded = { fg = "green_dark" },
-    OilGitModified = { fg = "yellow_dark" },
-    OilGitRenamed = { fg = "blue_dark" },
-    OilGitDeleted = { fg = "red_dark" },
-    OilGitCopied = { fg = "blue_dark" },
-    OilGitConflict = { fg = "purple_dark" },
-    OilGitUntracked = { fg = "bg2" },
-    OilGitIgnored = { fg = "gray" },
+    OilGitUntracked = { fg = "gray" },
   },
 })
 
